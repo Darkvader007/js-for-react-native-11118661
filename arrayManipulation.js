0c1386635a26ada2d54d6f8f8bb9b76d72d00413
Gabriel Addo <dekimelz@gmail.com>
2024-05-19 02:07:25 +0000

    Done with Task 2

--- arrayManipulation.js
+++ arrayManipulation.js
@@ -7,3 +7,21 @@ function processArray (arr){
  }});
 }  
  
+function formatArrayStrings(strings,processedNumbers){
+    return processedNumbers.map((num,index)=> {
+        if (num % 2 === 0){
+            return strings[index].toUpperCase();
+        } else{
+            return strings[index].toLowerCase();
+        }
+    }
+    
+    
+    
+    );
+}
+
+const strings = ['Gabby','Rollas','Osborn','Icon'];
+const processedNumbers =[1,2,3,4];
+const formattedStrings = formatArrayStrings(strings,processedNumbers);
+console.log(formattedStrings);
